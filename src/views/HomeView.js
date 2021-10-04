2204de7058f53437333f403edeb9fa98f6608437
Scott Stossel <stoss96@gmail.com>
2021-10-04 11:40:48 -0400

    fixed homepage rendering

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,8 +2,23 @@ import "./HomeView.css";
 import FeaturedCard from "../components/FeaturedCard";
 import PostCard from "../components/PostCard";
 import { Row, Col, Container } from "react-bootstrap";
+import { getPostsFromApi } from "../services/postServices";
+import { useEffect, useState } from "react";
 
 const HomeView = () => {
+  const [posts, setPosts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    const response = await getPostsFromApi();
+    setPosts(response.data);
+    console.log(response.data);
+  }
+
   return (
     <div className="home-view">
       <div className="home">
@@ -18,17 +33,15 @@ const HomeView = () => {
         <FeaturedCard />
       </div>
       <Container>
-        <Row xs={4} md={2} className="g-4">
-          <Col>
-            <PostCard />
-          </Col>
-          <Col>
-            <PostCard />
-          </Col>
-          <Col>
-            <PostCard />
-          </Col>
-        </Row>
+      <Row xs={4} md={2} className="g-4">
+        {posts && posts.map((post) => (
+          <div key={post._id}>
+            <Col>
+              <PostCard post={post} />
+            </Col>
+          </div>
+        ))}
+      </Row>
       </Container>
     </div>
   );
